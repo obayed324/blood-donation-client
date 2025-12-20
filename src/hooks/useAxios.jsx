1b051037ba0c80fafa386cf2a7fe729b431d6435
obayed324 <23-52090-2@student.aiub.edu>
2025-12-20 18:58:47 +0600

    add base url from firebase

--- src/hooks/useAxios.jsx
+++ src/hooks/useAxios.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:3000'
+    baseURL: 'https://blood-donation-server-mu.vercel.app'
 })
 
 const useAxios = () => {
